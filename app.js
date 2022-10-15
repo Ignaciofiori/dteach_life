932c16b9e07faebf04d79e7a68b0421188e6bdf1
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2022-10-15 12:09:46 -0300

    se agregaron iconos awesome 6.2.0, se configuro header y footer

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/index.html"));
+  res.sendFile(path.join(__dirname, "./views/home.html"));
 });
-app.get("/productDetail.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/productDetail.html"));
-});
-
-app.get("/productCart.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/productCart.html"));
+app.get("/product", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/product.html"));
 });
 
-app.get("/register.html", (req, res) => {
+app.get("/cart", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/cart.html"));
+});
+
+app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/register.html"));
 });
 
-app.get("/login.html", (req, res) => {
-  res.sendFile(path.join(--dirname, "./views/login.html"));
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
 });
