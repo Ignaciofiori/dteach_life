e88f11ca2d0a39af861ed28e5d57553cf387773a
ignaciofiori <nacho.fiori@hotmail.com.ar>
2022-11-11 19:25:50 -0300

    modifique header para que tenga opcion de agrgar productos/clases

---
+++ src/database/products.js
@@ -0,0 +1,25 @@
+const profesores = [
+  {
+    id: '1',
+    name: 'Ricardo Villalobos',
+    ubicacion: 'Beccar, San Isidro',
+    descripcion: 'Sociable y dinamico entrenamiento de futbol',
+    precio: '500$',
+  },
+  {
+    id: '2',
+    name: 'Juan Abate',
+    ubicacion: 'Flores, CABA',
+    descripcion: 'Entrenamiento de preparacion fisica de futbol',
+    precio: '700$',
+  },
+  {
+    id: '3',
+    name: 'Alejandro Broggi',
+    ubicacion: 'Olivos, Vicente lopez',
+    descripcion: 'Entrenamiento recreativo de futbol',
+    precio: '500$',
+  },
+];
+
+module.exports = profesores;
