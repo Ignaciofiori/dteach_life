fe6fdcf1dcb0bae2976495b5caffb028b5a06112
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2022-11-16 12:25:19 -0300

    se actualizaron rutas de edit y detail

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.index);
-router.get('/detail/:id', productsController.detail);
+router.get('/:id/detail', productsController.detail);
 router.get('/cart', productsController.cart);
 router.get('/create', productsController.createForm);
 router.post('/create', productsController.create);
-router.get('/edit/:id', productsController.editForm);
-router.put('/edit/:id', productsController.edit);
+router.get('/:id/edit', productsController.editForm);
+router.put('/:id/edit', productsController.edit);
 
 module.exports = router;
