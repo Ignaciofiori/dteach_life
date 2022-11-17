c551b3e792e3bbf1dec481415d8a849301e65b8e
ignaciofiori <nacho.fiori@hotmail.com.ar>
2022-11-16 22:30:43 -0300

    correccion de vista en productos y se agrego estructura de archivos de los users

---
+++ src/controllers/usersController.js
@@ -0,0 +1,20 @@
+const usersController={
+    index:(req,res)=>{
+res.render("users/index")
+    },
+    createUser:(req,res)=> {
+        let user ={
+          id: users.length + 1,
+          nombre: req.body.nombre + " " + req.body.apellido,
+          ubicacion: req.body.ubicacion,
+          imagen: "",
+          email: req.body.email,
+          contraseña: req.body.contraseña,
+        }
+       users.push(user);
+       let usersJSON =JSON.stringify(users);
+       fs.writeFileSync(usersFilePath,usersJSON);
+       res.redirect("/")
+      }
+    }
+module.exports= usersController
