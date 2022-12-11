55756f94863cc6333941b2854e5447125ad707dc
ROMO1985 <romulo.coll@gmail.com>
2022-12-11 10:33:32 -0300

    spring 5 - Multer en Usuaruio _ validador: primero para los campos si estan vacios _ Falta el msj: que lo imprima en la vista, va ser el proximo commit

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,10 @@
+//Falta terminar el validador: falta que envie el msj y el criptador de contraseña.-
+
 const fs = require("fs")
 const path =require("path")
 const usersFilePath = path.join(__dirname, '../database/users.json')
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
+ const {validationResult}= require('express-validator')
 
 const usersController={
     index:(req,res)=>{
@@ -11,6 +14,7 @@ res.render("users/index")
         res.render('users/login');
       },
       register: (req, res) => {
+
         res.render('users/register');
       },
     createUser:(req,res)=> {
@@ -18,14 +22,30 @@ res.render("users/index")
           id: users.length + 1,
           nombre: req.body.nombre + " " + req.body.apellido,
           ubicacion: req.body.ubicacion,
-          imagen: "",
+          imagen: req.file.filename,
           email: req.body.email,
-          contraseña: req.body.contraseña,
+          contraseña: req.body.password,
         }
        users.push(user);
        let usersJSON =JSON.stringify(users);
        fs.writeFileSync(usersFilePath,usersJSON);
-       res.redirect("/")
+       console.log(users)
+
+       /*Importante estan los validadores de formulario, por eso no llega esta instancia,
+       pero lo configuro por el spring => pendiente enviar el msj
+       
+       */
+       const resultadoValidado =  validationResult(req)
+       
+       if(resultadoValidado.errors.length>0){
+        return  res.render('users/register',
+        {errors: resultadoValidado.mapped()
+        })
+        
+       } else{
+        res.send('Argentina Campeaon')
+       }
+
       }
     }
 module.exports= usersController
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const usersController = require("../controllers/usersController");
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const {body}= require('express-validator');
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/images/users'));
+    },
+    filename: function (req, file, cb) {
+      let nombreArchivo =  Date.now() +'imguser' +path.extname(file.originalname);
+        cb(
+        null,      
+        nombreArchivo
+        );
+    },
+  });
+  
+  const uploadFile = multer({ storage: storage });
+  
+
+  const validaciones = [
+    body('nombre').notEmpty().withMessage('falta el Nombre'),
+    body('apellido').notEmpty().withMessage('falta el Apellido'),
+    body('ubicacion').notEmpty().withMessage('falta la ubicación'),
+    body('email').notEmpty().withMessage('falta el email'),
+    body('password').notEmpty().withMessage('falta el password'),
+  ]
+
+
 
 router.get("/",usersController.index);
 router.get('/login', usersController.login)
 router.get('/register', usersController.register)
-router.post("/register",usersController.createUser)
+router.post("/register",uploadFile.single('imagenUsuario'),validaciones,usersController.createUser)
 
 module.exports = router
