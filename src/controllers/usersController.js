45539a092b227ae885932be45d3ca2ce9f0ae7c8
ROMO1985 <romulo.coll@gmail.com>
2022-12-11 21:06:19 -0300

    agrego los comentarios de las validaciones, tb que las imagenes

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -18,34 +18,37 @@ res.render("users/index")
         res.render('users/register');
       },
     createUser:(req,res)=> {
+
+      
+       const resultadoValidado =  validationResult(req)
+       
+       if(resultadoValidado.errors.length>0){
+
+        console.log(resultadoValidado.errors)
+
+        return  res.render('users/register',
+        {errors: resultadoValidado.mapped()})
+        
+       } else{
+
         let user ={
           id: users.length + 1,
           nombre: req.body.nombre + " " + req.body.apellido,
           ubicacion: req.body.ubicacion,
           imagen: req.file.filename,
           email: req.body.email,
-          contraseña: req.body.password,
+          contraseña: req.body.password
         }
        users.push(user);
        let usersJSON =JSON.stringify(users);
        fs.writeFileSync(usersFilePath,usersJSON);
-       console.log(users)
 
-       /*Importante estan los validadores de formulario, por eso no llega esta instancia,
-       pero lo configuro por el spring => pendiente enviar el msj
-       
-       */
-       const resultadoValidado =  validationResult(req)
-       
-       if(resultadoValidado.errors.length>0){
-        return  res.render('users/register',
-        {errors: resultadoValidado.mapped()
-        })
+       return  res.render('users/login');
+
+
         
-       } else{
-        res.send('Argentina Campeaon')
        }
 
-      }
+    }
     }
 module.exports= usersController
--- src/routes/users.js
+++ src/routes/users.js
@@ -27,9 +27,23 @@ const storage = multer.diskStorage({
     body('nombre').notEmpty().withMessage('falta el Nombre'),
     body('apellido').notEmpty().withMessage('falta el Apellido'),
     body('ubicacion').notEmpty().withMessage('falta la ubicación'),
-    body('email').notEmpty().withMessage('falta el email'),
+    body('email')
+      .notEmpty().withMessage('falta el email').bail()
+      .isEmail().withMessage('tiene que ser un mail'),
+
     body('password').notEmpty().withMessage('falta el password'),
-  ]
+    body('imagenUsuario').custom((value,{req}) =>{
+    let file = req.file;
+    let acceptedExtensions = ['.jpg','.png','.gif'];
+    if(!file){
+      throw new Error('tiene que subir una imagen');
+    }else{
+      let fileExtension = path.extname(file.originalname)
+      if(!acceptedExtensions.includes(fileExtension)){
+      throw new Error ('Las extensiones nos compatibles')
+    }}
+      return true;
+    })]
 
 
 
