6c2c804caf531772eefe00b79c139e913d5d06bb
ignaciofiori <nacho.fiori@hotmail.com.ar>
2022-12-12 20:25:02 -0300

    rutas

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,5 +13,6 @@ router.get("/",usersController.index);
 router.get('/login', usersController.login)
 router.get('/register', usersController.register)
 router.post("/register",uploadFile.single('imagenUsuario'),validaciones ,usersController.createUser)
-
+router.post("/login",usersController.loginProcess)
+router.get("/profile",usersController.profile)
 module.exports = router
