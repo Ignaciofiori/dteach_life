456f2a18b17f8fe71bf6f6fe672a8be20885f057
ignaciofiori <nacho.fiori@hotmail.com.ar>
2022-12-12 22:38:04 -0300

    agregue middlewares de autentificacion y guest, para que si el usuario no este logueado no pueda ingresar al profile, y otro para que si si esta logueado que no pueda ingresar al login ni al register

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,13 +6,13 @@ const {body}= require('express-validator');
   
 //MIDDLEWARES 
 const uploadFile = require("../middlewares/userMulterMiddleware")
-const validaciones =require("../middlewares/validacionesRegisterMiddleware")
-
-
+const validaciones = require("../middlewares/validacionesRegisterMiddleware")
+const guestMiddleware = require("../middlewares/guestMiddleware")
+const authMiddleware = require("../middlewares/authMiddleware")
 router.get("/",usersController.index);
-router.get('/login', usersController.login)
-router.get('/register', usersController.register)
+router.get('/login',guestMiddleware ,usersController.login)
+router.get('/register', guestMiddleware, usersController.register)
 router.post("/register",uploadFile.single('imagenUsuario'),validaciones ,usersController.createUser)
 router.post("/login",usersController.loginProcess)
-router.get("/profile",usersController.profile)
+router.get("/profile",authMiddleware,usersController.profile)
 module.exports = router
