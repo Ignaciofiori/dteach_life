37c6baf6b8a584b223c76950d0b566d3107b766a
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2022-12-23 13:58:59 -0300

    se agregaron validaciones y correcciones en los formularios de login y creacion

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,21 +1,53 @@
+const User = require('../models/User');
 const fs = require('fs');
 const path = require('path');
 const { validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
 const usersFilePath = path.join(__dirname, '../database/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController = {
   profile: (req, res) => {
-    res.render('users/profile');
+    res.render('users/profile', {
+      user: req.session.userLogged,
+    });
   },
   login: (req, res) => {
     res.render('users/login');
   },
   loginProcess: (req, res) => {
-    console.log('loginProcess');
+    const errors = validationResult(req);
+    console.log(errors);
+    let userALoguear = User.findByField('email', req.body.email);
+    if (userALoguear) {
+      let contraseñaCorrecta = bcryptjs.compareSync(
+        req.body.password,
+        userALoguear.password
+      );
+      if (contraseñaCorrecta) {
+        delete userALoguear.password;
+        req.session.userLogged = userALoguear;
+        res.redirect('/users/profile');
+      }
+      return res.render('users/login', {
+        errors: [
+          {
+            msg: 'Las Credenciales son Invalidas',
+          },
+        ],
+      });
+    }
+    return res.render('users/login', {
+      errors: [
+        {
+          msg: 'No se encuentra este email en nuestra base de datos',
+        },
+      ],
+    });
   },
   logout: (req, res) => {
-    console.log('logout');
+    req.session.destroy();
+    res.redirect('/');
   },
   register: (req, res) => {
     res.render('users/register');
@@ -23,23 +55,29 @@ const usersController = {
   createUser: (req, res) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-      const user = {
-        id: users.length + 1,
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        ubicacion: req.body.ubicacion,
-        descripcion: req.body.descripcion,
-        imagen: req.file ? req.file.filename : null,
-        email: req.body.email,
-        password: req.body.password,
-      };
-      users.push(user);
-      const usersJSON = JSON.stringify(users);
-      fs.writeFileSync(usersFilePath, usersJSON);
-      res.redirect('/');
+      let userInDb = User.findByField('email', req.body.email);
+
+      if (userInDb) {
+        return res.render('users/register', {
+          errors: [{ msg: 'este email ya esta registrado' }],
+          oldData: req.body,
+        });
+      } else {
+        let userToCreate = {
+          ...req.body,
+          password: bcryptjs.hashSync(req.body.password, 10),
+          imagen: req.file.filename,
+        };
+
+        User.create(userToCreate);
+      }
     } else {
-      res.render('users/register', { errors: errors.array(), old: req.body });
+      res.render('users/register', {
+        errors: errors.array(),
+        oldData: req.body,
+      });
     }
+    res.redirect('/users/login');
   },
 };
 module.exports = usersController;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const multer = require('multer');
 const path = require('path');
 const usersController = require('../controllers/usersController');
 const { body } = require('express-validator');
+const guestMiddleware = require('../middlewares/guestMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // Configuracion de Multer
 const storage = multer.diskStorage({
@@ -40,40 +41,16 @@ const validateRegisterForm = [
     .withMessage('La clave debe tener minimo 6 caracteres.'),
 ];
 
-// router.get('/:id', usersController.profile);
-router.get('/login', usersController.login);
-router.get('/register', usersController.register);
+router.get('/profile', authMiddleware, usersController.profile);
+router.get('/login', guestMiddleware, usersController.login);
+router.post('/login', validateRegisterForm, usersController.loginProcess);
+router.get('/logout', usersController.logout);
+router.get('/register', guestMiddleware, usersController.register);
 router.post(
   '/register',
   upload.single('imagen'),
   validateRegisterForm,
   usersController.createUser
 );
-// router.get('/edit/:id', usersController.profile);
-// router.put(
-//   '/edit/:id',
-//   validateRegisterForm,
-//   upload.single('imagen'),
-//   usersController.index
-// );
-// router.delete('/delete/:id', usersController.profile);
 
 module.exports = router;
-=======
-const path = require('path')
-const {body}= require('express-validator');
-  
-//MIDDLEWARES 
-const uploadFile = require("../middlewares/userMulterMiddleware")
-const validaciones = require("../middlewares/validacionesRegisterMiddleware")
-const guestMiddleware = require("../middlewares/guestMiddleware")
-const authMiddleware = require("../middlewares/authMiddleware")
-router.get("/",usersController.index);
-router.get('/login',guestMiddleware ,usersController.login)
-router.get('/register', guestMiddleware, usersController.register)
-router.post("/register",uploadFile.single('imagenUsuario'),validaciones ,usersController.createUser)
-router.post("/login",usersController.loginProcess)
-router.get("/profile",authMiddleware,usersController.profile)
-router.get("/logout",usersController.logout)
-module.exports = router
->>>>>>> a5495fe62cb799f9fc1dd41bdf2bf45c9f228004
