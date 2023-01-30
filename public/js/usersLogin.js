f21c906c309d82fbc0852b880d8b0415fc446c14
ignaciofiori <nacho.fiori@hotmail.com.ar>
2023-01-30 18:53:25 -0300

    complete validacion de frontend del formulario de login

---
+++ public/js/usersLogin.js
@@ -0,0 +1,44 @@
+window.addEventListener("load",()=>{
+
+    const formularioLogin =document.getElementById("formularioLogin") 
+
+formularioLogin.addEventListener("submit",(e)=>{
+//VALIDACION EMAIL
+    let erroresEmail = []
+    let campoEmail = document.getElementById("campoEmail")
+    
+    if(campoEmail.value == ""){
+    erroresEmail.push("El Email es obligatorio")
+    }
+    
+    if(erroresEmail.length > 0){
+        e.preventDefault()
+        let ulErroresEmail= document.querySelector(".email ul")
+        for(let i=0 ;i<erroresEmail.length;i++){
+            ulErroresEmail.innerHTML="<li>"+erroresEmail[i]+"</li>"
+        }
+        campoEmail.style.border="red solid 3px"
+    }
+//VALIDACION CONTRASEÑA
+let erroresPassword = []
+let campoPassword = document.getElementById("campoPassword")
+if(campoPassword.value == ""){
+erroresPassword.push("La Contraseña es obligatoria")
+
+}
+
+if(erroresPassword.length > 0){
+    e.preventDefault()
+    let ulErroresPassword= document.querySelector(".password ul")
+    for(let i=0 ;i<erroresPassword.length;i++){
+        ulErroresPassword.innerHTML="<li>"+erroresPassword[i]+"</li>"
+    }
+    campoPassword.style.border="red solid 3px"
+}
+})
+
+
+
+
+
+})
